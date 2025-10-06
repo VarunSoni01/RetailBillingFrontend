55b19e8524d2fdaec2250200f2525232c29b1921
VarunSoni01 <varunsoni448@gmail.com>
2025-10-06 13:56:47 +0530

    created explore container frame

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,6 +1,27 @@
+import './Explore.css';
+
+
 const Explore = () => {
     return (
-        <div>Explore</div>
+        <div className="explore-container text-light">
+            <div className="left-column">
+                <div className="first-row" style={{ overflowY: 'auto' }}></div>
+                <hr className="horizontal-line" />
+                <div className="second-row" style={{ overflowY: 'auto' }}></div>
+            </div>
+            <div className="right-column d-flex flex-column">
+                <div className="customer-form-container" style={{ height: '15%' }}>
+                    Customer Form
+                </div>
+                <hr className="my-3 text-light" /> {/*Margin y*/}
+                <div className="cart-items-container" style={{ height: '55%', overflowY: 'auto' }}>
+                    Cart Items
+                </div>
+                <div className="cart-summary-container" style={{ height: '30%' }}>
+                    Cart Summary
+                </div>
+            </div>
+        </div>
     )
 }
 
