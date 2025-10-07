9a707278db9bffd6917de96929113c0cc6537657
VarunSoni01 <varunsoni448@gmail.com>
2025-10-07 18:10:23 +0530

    checkpoint

--- src/components/CategoryForm/CategoryForm.jsx
+++ src/components/CategoryForm/CategoryForm.jsx
@@ -1,25 +1,26 @@
+import { useState } from "react";
+
 const CategoryForm = () => {
+    const [loading, setLoading] = useState(false);
+    const [image, setImage] = useState(false);
+    const [data, setData] = useState({
+        name: "",
+        description: "",
+        bgColor: "#ffffff"
+    });
     return (
         <div className="mx-2 mt-2">
             <div className="row">
-                <div className="card col-md-8 form-container">
+                <div className="card col-md-12 form-container">
                     <div className="card-body">
                         <form>
                             <div className="mb-3">
-                                <label htmlFor="image" className="form-lable"><img src="https://placehold.co/48x48" alt="" width={48} /></label>
-                                <input type="file" name="image" id="image" className="form-control" hidden />
-                            </div>
-                            <div className="mb-3">
                                 <label htmlFor="name" className="form-label">Name</label>
-                                <input type="text" name="name" id="name" className="form-control" placeholder="Category Name" />
+                                <input type="text" name="name" id="name" className="form-control" placeholder="Sample Name" />
                             </div>
                             <div className="mb-3">
-                                <label htmlFor="email" className="form-label">Email</label>
-                                <input type="email" name="email" id="email" className="form-control" placeholder="name@example.com" />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="password" className="form-label">Password</label>
-                                <input type="password" name="password" id="password" className="form-control" placeholder="********" />
+                                <label htmlFor="description" className="form-label">Description</label>
+                                <textarea rows={5} name="description" id="description" className="form-control" placeholder="Write content here: " />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="bgcolor" className="form-label">Background Color</label>
--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -2,16 +2,24 @@ const UserForm = () => {
     return (
         <div className="mx-2 mt-2">
             <div className="row">
-                <div className="card col-md-8 form-container">
+                <div className="card col-md-12 form-container">
                     <div className="card-body">
                         <form>
                             <div className="mb-3">
-                                <label htmlFor="name" className="form-label">Name</label>
-                                <input type="text" name="name" id="name" className="form-control" placeholder="Sample Name" />
+                                <label htmlFor="image" className="form-lable"><img src="https://placehold.co/48x48" alt="" width={48} /></label>
+                                <input type="file" name="image" id="image" className="form-control" hidden />
                             </div>
                             <div className="mb-3">
-                                <label htmlFor="description" className="form-label">Description</label>
-                                <textarea rows={5} name="description" id="description" className="form-control" placeholder="Write content here: " />
+                                <label htmlFor="name" className="form-label">Name</label>
+                                <input type="text" name="name" id="name" className="form-control" placeholder="Category Name" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="email" className="form-label">Email</label>
+                                <input type="email" name="email" id="email" className="form-control" placeholder="name@example.com" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="password" className="form-label">Password</label>
+                                <input type="password" name="password" id="password" className="form-control" placeholder="********" />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="bgcolor" className="form-label">Background Color</label>
