30f7a74c16429f22b0b679f2ee2bef3bad504d75
VarunSoni01 <varunsoni448@gmail.com>
2025-10-11 16:07:37 +0530

    added authorization in categories api

--- src/service/CategoryService.js
+++ src/service/CategoryService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const addCategory = async (category) => {
-    return await axios.post("http://localhost:8080/api/v1.0/admin/categories/addCategory", category);
+    return await axios.post("http://localhost:8080/api/v1.0/admin/categories/addCategory", category, { headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` } });
 }
 
 export const deleteCategory = async (categoryId) => {
-    return await axios.delete("http://localhost:8080/api/v1.0/admin/categories/deleteCategory/" + categoryId);
+    return await axios.delete("http://localhost:8080/api/v1.0/admin/categories/deleteCategory/" + categoryId, { headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` } });
 }
 
 export const fetchCategories = async () => {
-    return await axios.get("http://localhost:8080/api/v1.0/categories");
+    return await axios.get("http://localhost:8080/api/v1.0/categories", { headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` } });
 }
