7605f753d347d806b9833ee977cef427a250173f
VarunSoni01 <varunsoni448@gmail.com>
2025-10-14 08:01:42 +0530

    added profile menu option

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,9 +1,11 @@
 import logo from './logo.png';
 import uploads from './uploads.png'
 import login from './login-bg.png'
+import profileImg from './profile-img.png'
 
 export const assets = {
     logo,
     uploads,
-    login
+    login,
+    profileImg
 }
--- src/components/Menubar/Menubar.jsx
+++ src/components/Menubar/Menubar.jsx
@@ -1,8 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { assets } from '../../assets/assets';
 import './Menubar.css';
+import { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
 
 const Menubar = () => {
+
+    const { setAuthData } = useContext(AppContext);
+    const navigate = useNavigate();
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('role');
+
+        setAuthData(null, null);
+        navigate("/login");
+    }
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-2">
             <a className="navbar-brand" href="#">
@@ -29,7 +43,24 @@ const Menubar = () => {
                         <Link className="nav-link" to="/users">Manage Users</Link>
                     </li>
                 </ul>
-                {/* Add the user profile dropdwon here */}
+                {/* Add the user profile dropdown here */}
+                <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                    <li className="nav-item dropdown">
+                        <a href="#" className="nav-link dropdown-toggle" id='navbarDropdown' role='button' data-bs-toggle="dropdown" area-expanded="false">
+                            <img src={assets.profileImg} alt="" height={32} width={32} />
+                        </a>
+                        <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <li>
+                                <a href="#!" className='dropdown-item'>Settings</a>
+                                <a href="#!" className='dropdown-item'>Activity Log</a>
+                                <li>
+                                    <hr className="dropdown-divider" />
+                                </li>
+                                <a href="#!" className='dropdown-item' onClick={logout}>Logout</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
