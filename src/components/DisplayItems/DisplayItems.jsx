87d62b64340b0a96cd3a0c9b339b10d38cb7d824
VarunSoni01 <varunsoni448@gmail.com>
2025-10-14 18:40:50 +0530

    Added items in explore

--- src/components/DisplayItems/DisplayItems.jsx
+++ src/components/DisplayItems/DisplayItems.jsx
@@ -1,8 +1,25 @@
+import { useContext } from 'react';
 import './DisplayItems.css'
+import { AppContext } from '../../context/AppContext'
+import Item from '../Item/Item';
 
 const DisplayItems = () => {
+    const { items } = useContext(AppContext);
     return (
-        <div>Display Items</div>
+        <div className="p-3">
+            <div className="row g-3">
+                {items.map((item, index) => (
+                    <div key={index} className='col-md-4 col-sm-6'>
+                        <Item
+                            itemName={item.name}
+                            itemPrice={item.price}
+                            itemImage={item.imgUrl}
+                            itemId={item.itemId}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
