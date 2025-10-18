d5373aa3857eb9bef884a324130942e83c9171b9
VarunSoni01 <varunsoni448@gmail.com>
2025-10-18 15:59:19 +0530

    checkpoint

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -2,7 +2,14 @@ import './CustomerForm.css'
 
 const CustomerForm = () => {
     return (
-        <div>Customer Form</div>
+        <div className="p-3">
+            <div className="mb-3">
+                <div className="d-flex align-items-center gap-2">
+                    <label htmlFor="customerName" className='col-4'>Customer Name</label>
+                    <input type="text" className='form-control form-control-sm' id='customerName' on />
+                </div>
+            </div>
+        </div>
     )
 }
 
