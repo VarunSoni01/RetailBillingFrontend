4e209d7208ea933cd7ff10cd3d4a5a0b3779d415
VarunSoni01 <varunsoni448@gmail.com>
2025-10-20 06:05:19 +0530

    Created cart sumarry section

--- src/components/CartSummary/CartSummary.jsx
+++ src/components/CartSummary/CartSummary.jsx
@@ -1,8 +1,39 @@
+import { useContext } from 'react';
 import './CartSummary.css'
+import { AppContext } from '../../context/AppContext';
+import ReceiptPopup from '../ReceiptPopup/ReceiptPopup';
 
-const CartSummary = () => {
+const CartSummary = ({ customerName, mobileNumber, setCustomerName, setMobileNumber }) => {
+
+    const { cartItems } = useContext(AppContext);
+    const totalAmount = cartItems.reduce((total, item) => total + (item.price * item.quantity), 0);
+    const tax = totalAmount * 0.01;
+    const grandTotal = totalAmount + tax;
+
     return (
-        <div>Cart Summary</div>
+        <div className="mt-2">
+            <div className="cart-summary-details">
+                <div className="d-flex justify-content-between mb-2">
+                    <span className="text-light">Item: </span>
+                    <span className="text-light">₹{totalAmount.toFixed(2)}</span>
+                </div>
+                <div className="d-flex justify-content-between mb-2">
+                    <span className="text-light">Tax (1%): </span>
+                    <span className="text-light">₹{tax.toFixed(2)}</span>
+                </div>
+                <div className="d-flex justify-content-between mb-4">
+                    <span className="text-light">Total: </span>
+                    <span className="text-light">₹{grandTotal.toFixed(2)}</span>
+                </div>
+            </div>
+
+            <div className="d-flex gap-3">
+                <button className="btn btn-success flex-grow-1">Cash</button>
+                <button className="btn btn-primary flex-grow-1">UPI</button>
+            </div>
+
+            <ReceiptPopup />
+        </div >
     )
 }
 
---
+++ src/components/ReceiptPopup/ReceiptPopup.jsx
@@ -0,0 +1,10 @@
+import './ReceiptPopup.css';
+
+
+const ReceiptPopup = () => {
+    return (
+        <div>Receipt Popup</div>
+    )
+}
+
+export default ReceiptPopup;
