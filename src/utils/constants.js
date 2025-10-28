a4ea24315ec1e4f080d20d09fadcf523cdd1c5a9
VarunSoni01 <varunsoni448@gmail.com>
2025-10-28 18:30:16 +0530

    changed backend api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,5 @@ export const AppConstants = {
     RAZORPAY_KEY_ID: "rzp_test_RW3NbP1yT4Zh7e"
 };
 export const ApiPaths = {
-    BASE_URL: "http://localhost:8080/api/v1.0"
+    BASE_URL: "https://retail-billing-backend.onrender.com/api/v1.0"
 };
